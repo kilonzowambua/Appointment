cacdc7c3d8723055d9c5ed51d0b48945ea59b2eb
Anatoli <anatoli@anatoli.home>
2022-11-10 11:11:50 +0300

    First inilatation

---
+++ partial/navbar.php
@@ -0,0 +1,71 @@
+<nav class="sidebar dark_sidebar vertical-scroll  ps-container ps-theme-default ps-active-y">
+        <div class="logo d-flex justify-content-between">
+            <a href="index-2.html"><img src="img/logo_white.png" alt=""></a>
+            <div class="sidebar_close_icon d-lg-none">
+                <i class="ti-close"></i>
+            </div>
+        </div>
+        <ul id="sidebar_menu">
+            <li class="mm-active">
+                <a class="has-arrow" href="#" aria-expanded="false">
+                <i class="ti-dashboard"></i>
+                    <span>Dashboard</span>
+                </a>
+                
+            </li>
+            <li class="">
+                <a class="has-arrow" href="#" aria-expanded="false">
+                <i class="ti-user"></i>
+                    <span>User</span>
+                </a>
+                
+            </li>
+            <li class="">
+                <a class="has-arrow" href="#" aria-expanded="false">
+                <i class="ti-user"></i>
+                    <span>Clients</span>
+                </a>
+               
+            </li>
+            <li class="">
+                <a class="has-arrow" href="#" aria-expanded="false">
+                <i class="ti-clipboard"></i>
+                    <span>Categories</span>
+                </a>
+               
+            </li>
+            <li class="">
+                <a class="has-arrow" href="#" aria-expanded="false">
+                <i class="ti-support"></i>
+                    <span>Doctor</span>
+                </a>
+                
+            </li>
+            <li class="">
+                <a class="has-arrow" href="#" aria-expanded="false">
+                <i class="ti-calendar"></i>
+                    <span>Appointments</span>
+                </a>
+                
+            </li>
+            <li class="">
+                <a class="has-arrow" href="#" aria-expanded="false">
+              
+                <i class="ti-credit-card"></i>
+                    <span>Transactions</span>
+                </a>
+                
+            </li>
+            <li class="">
+                <a class="has-arrow" href="#" aria-expanded="false">
+                <i class="ti-settings"></i>
+                    <span>General Setting</span>
+                </a>
+                <ul>
+                    <li><a href="">Mail Setup</a></li>
+                   
+                </ul>
+            </li>
+          
+        </ul>
+    </nav>
